d39911b90e5cf97fd85807fdac268b68d693015a
Kabaddi4 <ec2-user@ip-172-31-60-239.ec2.internal>
2021-11-18 14:22:27 +0000

    [Start]admin/customers/index

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,22 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    
+  end
+
+  def update
+  end
+
+  private
+  def customer_params
+  params.require(:customer).permit(:last_name, :first_name, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted, :created_at, :updated_at)
+  end
+
 end
