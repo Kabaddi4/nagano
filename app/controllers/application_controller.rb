7896dc8d3b45140f4ebc6b9877f83b64fe2ecdb0
Kabaddi4 <ec2-user@ip-172-31-60-239.ec2.internal>
2021-11-19 15:25:50 +0000

    [update]admin/views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  #case whenで条件設定
+  def after_sign_in_path_for(resource)
+    case resource
+    when Customer
+      customers_path
+    when Admin
+      admin_customers_path
+    end
+  end
 end
