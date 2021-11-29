087f69b57590df6187ea59e659219b05d9786658
Kabaddi4 <ec2-user@ip-172-31-60-239.ec2.internal>
2021-11-29 14:44:24 +0000

    [start]registration column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   #case whenで条件設定
   def after_sign_in_path_for(resource)
     case resource
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
     when Admin
       admin_customers_path
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name])
   end
 end
