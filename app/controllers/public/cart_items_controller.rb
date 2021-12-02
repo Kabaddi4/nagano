98fa83c3d918d8d296aa7e76170bfd0c70d5d763
Kabaddi4 <ec2-user@ip-172-31-60-239.ec2.internal>
2021-12-02 10:00:53 +0000

    [create]cart_item/index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,14 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
-    binding.pry
   end
 
   def create
-    cart_item = Item.find(cart_item_params[:item_id])
+    cart_item = CartItem.new
+    item = Item.find(params[:cart_item][:item_id])
+    cart_item.item_id = item.id
+    cart_item.customer_id = current_customer.id
+    cart_item.amount = params[:cart_item][:amount] #目からウロコ。直接取得できる。
     cart_item.save
     redirect_to items_path
   end
