6b31b02c4e6d5bea8a089efe99245a32eb709cf1
Kabaddi4 <ec2-user@ip-172-31-60-239.ec2.internal>
2021-12-11 14:05:55 +0000

    [create]order#confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -18,16 +18,27 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    binding.pry #if params[:order][:status] == 1
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.customer_address
-    @order.name = current_customer.last_name + current_customer.first_name
-    #elsif params[:order][:status] == 2
-    #@order.name = Address.find(params[:order][:registered]).name
+    if params[:order][:status] == 1
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.customer_address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:status] == 2
+      @order.postal_code = Address.find(params[:order][:registered]).postal_code
+      @order.address = Address.find(params[:order][:registered]).address
+      @order.name = Address.find(params[:order][:registered]).name
+    elsif params[:order][:status] == 3
+      new_address = current_customer.addresses.new(address_params)
+    end
+    @cart_items = current_customer.cart_item.all
+
   end
 
   private
   def order_params
     params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
+  def address_params
+    params.require(:order).permit(:address, :name, :postage)
   end
 end
